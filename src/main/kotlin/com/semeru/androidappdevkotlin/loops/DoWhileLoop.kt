9ce5504fef0e8ec2725151762abe6c62a9c95aae
tamp4n <tampan@gmail.com>
2025-06-19 08:57:22 +0700

    edit file DoWhileLoop.kt

---
+++ src/main/kotlin/com/semeru/androidappdevkotlin/loops/DoWhileLoop.kt
@@ -0,0 +1,21 @@
+package com.semeru.androidappdevkotlin.loops
+
+fun main() {
+    var number = 1
+    do {
+        println("Number: $number")
+        number++
+    } while (number <= 10)
+
+    println("======================")
+    var a = 5
+    val b = a++
+    println("a: $a")
+    println("b: $b")
+    val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
+    while (a > b) {
+        println(cars[2])
+        break
+    }
+
+}
