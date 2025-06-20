10e1c50422ff9e778c9d86ea8c4946d1fe76c265
tamp4n <tampan@gmail.com>
2025-06-20 18:03:52 +0700

    edit file FunctionExamples.kt (findMinimumNumber)

--- src/main/kotlin/com/semeru/androidappdevkotlin/function/FunctionExamples.kt
+++ src/main/kotlin/com/semeru/androidappdevkotlin/function/FunctionExamples.kt
@@ -4,8 +4,21 @@ fun main() {
 
     show(10, 20)
 
+    val result = add(10, 20)
+    println("Result sum = $result")
+
+    val minimumNumber = findMinimumNumber(10, 20)
+    println("Minimum number is $minimumNumber")
+
 }
 
 fun show(num1: Int, num2: Int) { // return type is Unit
     println("You entered $num1 and $num2")
 }
+
+fun add(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
+
+// return type is Int, return value is minimumNumber
+fun findMinimumNumber(num1: Int, num2: Int): Int = if (num1 < num2) num1 else num2
