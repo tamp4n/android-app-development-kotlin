5f425f026394c2c5c21dfdf56c913c75d057a132
tamp4n <tampan@gmail.com>
2025-06-20 17:51:11 +0700

    edit file FunctionExamples.kt (show)

---
+++ src/main/kotlin/com/semeru/androidappdevkotlin/function/FunctionExamples.kt
@@ -0,0 +1,11 @@
+package com.semeru.androidappdevkotlin.function
+
+fun main() {
+
+    show(10, 20)
+
+}
+
+fun show(num1: Int, num2: Int) { // return type is Unit
+    println("You entered $num1 and $num2")
+}
